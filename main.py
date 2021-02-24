484b8faabc27c8b51b65a21bb334b7ec815f348b
pse17 <fis777@yandex.ru>
2021-02-24 16:03:52 +0500

    fis flsake worninig

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello !'
--- test_main.py
+++ test_main.py
@@ -1,13 +1,13 @@
 import pytest
 import main
-import os
-import tempfile
+
 
 @pytest.fixture
 def client():
     main.app.config['TESTING'] = True
     with main.app.test_client() as client:
         yield client
+
 
 def test_home(client):
     resp = client.get('/')
