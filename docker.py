6c457726f47a19870743e84c7081b498ac863252
pse17 <fis777@yandex.ru>
2021-03-03 21:32:59 +0500

    czcz

--- docker.py
+++ docker.py
@@ -1,7 +1,13 @@
 import docker
 
-docker_client = docker.from_env()
-containers = docker_client.containers.list()
-container = docker_client.containers.get(container_name)
-container.kill()
-docker_client.containers.prune()
+client = docker.from_env()
+image = client.image.pull('igorf777/gitact', tag='main')
+client.containers.run(image, detach=True, ports=('0.0.0.0',5000), name='acts')
+
+#client = docker.from_env()
+
+#client.i
+#containers = docker_client.containers.list()
+#container = docker_client.containers.get(container_name)
+#container.kill()
+#docker_client.containers.prune()
